d81eda274eb7eeeaf45ed55c4889a33bfd967b4e
Console.log("Pinaki") <Pinaki.bpd2001@gmail.com>
2023-12-20 23:42:00 +0530

    basic calculation operations

---
+++ basic/calculator.py
@@ -0,0 +1,13 @@
+#python program to perform basic arithmatic calculation operation
+a = 10
+b = 5
+
+add = a+b
+multiply = a*b
+subtract = a-b
+division = a/b
+
+print("The addition is {}".format(add))
+print("The subtraction is {}".format(subtract))
+print("The Multiplication  is {}".format(multiply))
+print("The Division is {}".format(division))
