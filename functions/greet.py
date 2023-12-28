c050a7f5424e07c2b3c43d013df40f3b629bca3c
Console.log("Pinaki") <Pinaki.bpd2001@gmail.com>
2023-12-28 10:03:52 +0530

    updating greet function

--- functions/greet.py
+++ functions/greet.py
@@ -1,6 +1,12 @@
-def greet(name, department):
+# normal function with no parameters
+def greet1():
+    print("Good morning")
+
+# functions with parameters (parameterized functions)
+def greet2(name, department):
     print("welcome " + name)
     print("You are a part of " + department)
 
 
-greet("Rohan", "CSE")
+greet1()
+greet2("Rohan", "CSE")
