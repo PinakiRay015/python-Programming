07311794bef01e738842e930faa3ddff492ca4c2
Console.log("Pinaki") <Pinaki.bpd2001@gmail.com>
2023-12-28 09:47:36 +0530

    updating greet function

--- functions/greet.py
+++ functions/greet.py
@@ -1,5 +1,6 @@
-def greet(name):
-    print("Hello " + name)
+def greet(name, department):
+    print("welcome " + name)
+    print("You are from " + department + " department")
 
 
-greet("Rohan")
+greet("Rohan", "CSE")
