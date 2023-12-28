82d76b21f6d563abf515c61351a8261ee5e766fc
Console.log("Pinaki") <Pinaki.bpd2001@gmail.com>
2023-12-28 10:09:55 +0530

    adding of new function areaOfRectangle()

--- functions/greet.py
+++ functions/greet.py
@@ -2,11 +2,17 @@
 def greet1():
     print("Good morning")
 
+
 # functions with parameters (parameterized functions)
 def greet2(name, department):
     print("welcome " + name)
     print("You are a part of " + department)
 
 
+def areaOfRectangle(length, breadth):
+    return str(length * breadth)
+
+
 greet1()
 greet2("Rohan", "CSE")
+print("The area of rectangle is " + areaOfRectangle(10, 5))
