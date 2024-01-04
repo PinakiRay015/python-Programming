bb46dee80c6d7911e88acdfa47fcff04944cfa78
Console.log("Pinaki") <Pinaki.bpd2001@gmail.com>
2024-01-04 10:48:40 +0530

    using of and , or operator

---
+++ conditional/sample1.py
@@ -0,0 +1,8 @@
+# This statement will give 'true' as both the conditions are satisfying
+print(10 > 1 and 4 > 2)
+
+# This statement will give 'false' as both the conditions not satisfying
+print(10 > 1 and 4 < 2)
+
+# This statement will give 'true' as any one condition is satisfying
+print(10 > 1 or 4 < 2)
