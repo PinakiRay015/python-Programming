480a684307f41196a4fe23621db1b7afb63ae489
Console.log("Pinaki") <Pinaki.bpd2001@gmail.com>
2024-01-05 09:59:26 +0530

    using of logical operators

--- conditional/sample1.py
+++ conditional/sample1.py
@@ -1,8 +1,8 @@
-# This statement will give 'true' as both the conditions are satisfying
+# This statement will give 'true' as both the conditions are satisfying here
 print(10 > 1 and 4 > 2)
 
-# This statement will give 'false' as both the conditions not satisfying
+# This statement will give 'false' as both the conditions not satisfying here
 print(10 > 1 and 4 < 2)
 
-# This statement will give 'true' as any one condition is satisfying
+# This statement will give 'true' as any one condition is satisfying here
 print(10 > 1 or 4 < 2)
